f0c1d0d9540a643d11f8c06aa28ef12b53f2a3e8
VerasNp <vitorveras@proton.me>
2026-05-10 23:53:22 -0300

    Começa uma ligação entre rasterizados e desenhados pela API

--- src/geometry/Line.ts
+++ src/geometry/Line.ts
@@ -1,20 +1,26 @@
 import type Vector2 from "mwpjs/Vector2";
 import type IRenderer from "../renderer/IRenderer";
+import type { IDrawable } from "#scene/IDrawable.ts";
+import type Color from "#image/Color.ts";
 
-class Line {
+class Line implements IDrawable {
 	public p1: Vector2;
 	public p2: Vector2;
+	public color: Color;
 
-	constructor(p1: Vector2, p2: Vector2) {
+	constructor(p1: Vector2, p2: Vector2, color: Color) {
 		this.p1 = p1;
 		this.p2 = p2;
+		this.color = color;
 	}
 
 	public length(): number {
 		throw new Error("Method not implemented.");
 	}
 
-	
-
-	public draw(renderer: IRenderer): void {}
+	public draw(renderer: IRenderer): void {
+		renderer.drawLine(this.p1, this.p2);
+	}
 }
+
+export default Line;
--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,10 @@
-import Vector2 from "mwpjs/Vector2";
 import p5 from "p5";
-import ImageObject from "./geometry/ImageObject";
 import Color from "./image/Color";
 import P5Renderer from "./renderer/P5Renderer";
 import Scene from "./scene/Scene";
 import Image from "./image/Image";
-import { Pipeline } from "./pipeline/Pipeline";
-import CircleBresenham from "#rasterization/circle/CircleBresenham.ts";
-import EllipseInc from "#rasterization/ellipse/EllipseInc.ts";
+import Line from "#geometry/Line.ts";
+import Vector2 from "mwpjs/Vector2";
 
 const sketch = (p: p5) => {
 	let scene: Scene;
@@ -16,21 +13,18 @@ const sketch = (p: p5) => {
 	const PIXEL_SIZE = 20;
 	const IMG_COLS = Math.floor(CANVAS_WIDTH / PIXEL_SIZE);
 	const IMG_ROWS = Math.floor(CANVAS_HEIGHT / PIXEL_SIZE);
-	const pipeline = new Pipeline(undefined, undefined, new EllipseInc());
 	p.setup = () => {
 		p.createCanvas(CANVAS_WIDTH, CANVAS_HEIGHT);
 		const renderer = new P5Renderer(p);
 		scene = new Scene(renderer);
 		const canvas = new Image(IMG_COLS, IMG_ROWS, PIXEL_SIZE);
 		canvas.fill(new Color(30, 30, 80, 255));
-		pipeline.drawEllipse(
-			canvas,
-			new Vector2(IMG_COLS / 2, IMG_ROWS / 2),
-			10,
-			5,
-			new Color(200, 50, 50, 255),
+		const line = new Line(
+			new Vector2(10, 10),
+			new Vector2(30, 20),
+			new Color(255, 0, 0, 255),
 		);
-		scene.add(new ImageObject(canvas, new Vector2(0, 0)));
+		scene.add(line);
 	};
 
 	p.draw = () => {
